b56c84ad01b549b92ec497d1b0f0689ee22dccfc
Andreas_Sujono <andreassujono@gmail.com>
2020-02-17 02:15:30 +0800

    Updates

---
+++ precache-manifest.f3fcea4e0bca0979aff77a8cc7eae99a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b173343f48dd0cb5ac86c3cfc656023e",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "0225fdb55bf67446eac1",
+    "url": "/pomodoro-clock/static/css/main.2606bd2d.chunk.css"
+  },
+  {
+    "revision": "1d14e7d0457336e7d141",
+    "url": "/pomodoro-clock/static/js/2.8928e58d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pomodoro-clock/static/js/2.8928e58d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0225fdb55bf67446eac1",
+    "url": "/pomodoro-clock/static/js/main.3c8314c7.chunk.js"
+  },
+  {
+    "revision": "4cc8e959fe1d967d61ff",
+    "url": "/pomodoro-clock/static/js/runtime-main.c524c67b.js"
+  },
+  {
+    "revision": "ff3ed4e608fe0fb27bb3a0a1b894089a",
+    "url": "/pomodoro-clock/static/media/timerEnd.ff3ed4e6.mp3"
+  }
+]);
